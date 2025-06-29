4cd7a44d781a443f0642377f79af795a67b8b38b
resforxofficial <paedeogim@gmail.com>
2025-06-29 23:10:06 +0900

    refactoring

--- test/test.ts
+++ test/test.ts
@@ -1,7 +1,1 @@
-import { RelatedJSON, WantTo } from '../src/index';
-
-const rdb = new RelatedJSON();
-rdb.connect({ to: "../dist/test.json" });
-const wt = WantTo;
-
-rdb.create(wt.TABLE, "User", {});
+// refactoring
